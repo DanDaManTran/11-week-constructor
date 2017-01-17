20672909f63c749386515720ad5287d39f4c93d2
DanDaManTran <DannDoesntKnow@dans-mbp.home>
2017-01-17 12:16:11 -0600

    maybe I am doing itt right? -DT

--- shapes.js
+++ shapes.js
@@ -1,15 +1,21 @@
 
 
-function Shape (type) {
-	this.type = type;
-	this.get_type = function(){
-		return this.type;
-	};
+function Shape () {
+	this.type = undefined;
+
 };
 
-console.log(Shape.prototype);
+Shape.prototype.get_type = function(){
+	console.log("This shape type is " + this.type);
+};
+
+
+
+
 
 function Triangle (side1, side2, side3) {
+
+	this.type = "triangle";
 	this.side1 = side1;
 	this.side2 = side2;
 	this.side3 = side3;
