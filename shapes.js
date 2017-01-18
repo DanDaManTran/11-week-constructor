645746365477aac221810b7e29a72d154fcd5b28
DanDaManTran <DannDoesntKnow@Dans-MacBook-Pro.local>
2017-01-17 20:50:44 -0600

    hopefully finished? -DT

--- shapes.js
+++ shapes.js
@@ -1,4 +1,4 @@
-
+'use strict';
 //construtor function for shapes, double checks of we put new while creating a "new shape"
 function Shape (type) {
 	if(!(this instanceof Shape)){
@@ -6,7 +6,6 @@ function Shape (type) {
 	}
 
 	this.type = type;
-	this.length = 0;
 };
 
 //adding a get_type function to the Shape prototype which will return shape type
@@ -17,7 +16,7 @@ Shape.prototype.get_type = function() {
 //adding a get_totalLength function to the Shape prototype which will return the total length
 Shape.prototype.get_totalLength = function () {
 	var length = 0;
-	if (this.type == 'triangle') {
+	if (this.type === 'Triangle') {
 		length = this.side1 + this.side2 + this.side3;
 	} else if (this.type == 'Square') {
 		length = 4 * this.side;
@@ -31,10 +30,13 @@ function Square (side) {
 		return new Square (side);
 	}
 
-	var square = new Shape('Square');
-	square.side = side;
-	return square;
+	this.type = "Square";
+	this.side = side;
 };
+
+//slapping Shape stuff into Square 
+Square.prototype = new Shape();
+Square.prototype.construtor = Square;
 
 //construtor function for new triangle by creating a var triangle by using the Shape construtor and adding a side to the var and returning it to the new triangle.
 function Triangle (side1, side2, side3) {
@@ -42,20 +44,50 @@ function Triangle (side1, side2, side3) {
 		return new Triangle (side1, side2, side3);
 	}
 
-	var triangle = new Shape("triangle");
-	triangle.side1 = side1;
-	triangle.side2 = side2;
-	triangle.side3 = side3;
-	return triangle;
+	this.type = "Triangle";
+	this.side1 = side1;
+	this.side2 = side2;
+	this.side3 = side3;
 };
 
-//first triangle and sqaure 
+//slapping Shape stuff into Triangle 
+Triangle.prototype = new Shape();
+Triangle.prototype.construtor = Triangle;
+
+//construtor function for new Pentagon by creating a var triangle by using the Shape construtor and adding a side to the var and returning it to the new pentagon.
+function Pentagon (side1, side2, side3, side4, side5) {
+	if(!(this instanceof Pentagon)){
+		return new Pentagon (side1, side2, side3, side4, side5);
+	}
+
+	this.type = "Pentagon";
+	this.side1 = side1;
+	this.side2 = side2;
+	this.side3 = side3;
+	this.side4 = side4;
+	this.side5 = side5;
+};
+
+//slapping Shape stuff into Triangle 
+Pentagon.prototype = new Shape();
+Pentagon.prototype.construtor = Pentagon;
+
+
+//creating variables of each constructor
 var tri1 = new Triangle(3,4,5);
 var square1 = new Square(4);
+var pent1 = new Pentagon(1,2,3,4,5);
+var shape = new Shape("shape");
 
-//testing displaying information about the new tri and squares
-console.log("tri1 type:" + tri1.get_type());
-console.log("tri1 length:" + tri1.get_totalLength() + "end");
-console.log("square1 type:" + square1.get_type());
-console.log("square1 length:" + square1.get_totalLength());
 
+//getting different types
+console.log(tri1.get_type());
+console.log(square1.get_type());
+console.log(pent1.get_type());
+console.log(shape);
+
+// Ensuring instanceof works as expected.
+console.log(tri1 instanceof Triangle);
+console.log(square1 instanceof Square);
+console.log(pent1 instanceof Pentagon);
+console.log(shape instanceof Shape);
