f9290ec27c91ca2e8beef8f371932960cacfbb45
DanDaManTran <DannDoesntKnow@Dans-MacBook-Pro.local>
2017-01-17 19:23:33 -0600

    adding the comments for a better understanding -DT

--- shapes.js
+++ shapes.js
@@ -1,5 +1,5 @@
 
-
+//construtor function for shapes, double checks of we put new while creating a "new shape"
 function Shape (type) {
 	if(!(this instanceof Shape)){
 		return new Shape (type);
@@ -9,10 +9,12 @@ function Shape (type) {
 	this.length = 0;
 };
 
+//adding a get_type function to the Shape prototype which will return shape type
 Shape.prototype.get_type = function() {
 	return "This shape type is " + this.type;
 };
 
+//adding a get_totalLength function to the Shape prototype which will return the total length
 Shape.prototype.get_totalLength = function () {
 	var length = 0;
 	if (this.type == 'triangle') {
@@ -21,8 +23,9 @@ Shape.prototype.get_totalLength = function () {
 		length = 4 * this.side;
 	}
 	return length;
-}
+};
 
+//construtor function for new squares by creating a var square by using the Shape construtor and adding a side to the var and returning it to the new square.
 function Square (side) {
 	if(!(this instanceof Square)){
 		return new Square (side);
@@ -31,9 +34,9 @@ function Square (side) {
 	var square = new Shape('Square');
 	square.side = side;
 	return square;
-}
+};
 
-
+//construtor function for new triangle by creating a var triangle by using the Shape construtor and adding a side to the var and returning it to the new triangle.
 function Triangle (side1, side2, side3) {
 	if(!(this instanceof Triangle)){
 		return new Triangle (side1, side2, side3);
@@ -44,13 +47,15 @@ function Triangle (side1, side2, side3) {
 	triangle.side2 = side2;
 	triangle.side3 = side3;
 	return triangle;
-}
+};
 
+//first triangle and sqaure 
 var tri1 = new Triangle(3,4,5);
 var square1 = new Square(4);
 
-console.log("tri1 type:" + tri1.type);
+//testing displaying information about the new tri and squares
+console.log("tri1 type:" + tri1.get_type());
 console.log("tri1 length:" + tri1.get_totalLength() + "end");
-console.log("square1 type:" + square1.type);
+console.log("square1 type:" + square1.get_type());
 console.log("square1 length:" + square1.get_totalLength());
 
